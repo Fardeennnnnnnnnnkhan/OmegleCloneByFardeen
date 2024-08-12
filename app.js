0e10485d61aea396c5f8e8202dd5c5c837474248
Fardeen khan <fk7483136@gmail.com>
2024-08-12 20:17:15 +0530

    added process.env.PORT

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static(path.join(__dirname , "public")))
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes');
 
 app.use("/" , indexRouter);
 
